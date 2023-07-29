79a8e6c3c9c12c5a1919785559733d663985a015
l0st-idi0t <carlnod121@gmail.com>
2023-07-29 17:51:11 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
-import authClient from "./logic";
+import axios from "axios";
 
 const App = () => {
-  const [accessToken, setAccessToken] = useState(null);
-
   const responseMessage = (response) => {
-    setAccessToken(response.credentials);
+    localStorage.setItem("access_token", response.credentials);
+    console.log(response.credentials);
   };
   const errorMessage = (error) => {
     console.log(error);
+  };
+
+  const listDriveFiles = () => {
+    const accessToken = localStorage.getItem("access_token");
+
+    if (accessToken) {
+      axios
+        .get("https://www.googleapis.com/drive/v3/files", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((response) => {
+          console.log("Files:", response.data.files);
+        })
+        .catch((error) => {
+          console.error("Error listing files:", error);
+        });
+    }
   };
 
   return (
@@ -18,7 +36,12 @@ const App = () => {
       <h2>Download Your Drive Files</h2>
       <div className="Modal">
         <h3>Please sign in</h3>
-        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        <GoogleLogin
+          onSuccess={responseMessage}
+          onError={errorMessage}
+          scope="https://www.googleapis.com/auth/drive.readonly"
+        />
+        <button onClick={listDriveFiles}>List Drive Files</button>
       </div>
     </div>
   );
